6fef769e0b28d47c0eef4a9f7c7c85412ab520f9
mymovements <165665551+mymovements@users.noreply.github.com>
2024-04-03 00:14:56 +0200

    Add files via upload

---
+++ compte.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compte - CAL Bank</title>
+</head>
+<body>
+
+<div class="container">
+    <h1>Informations du Compte</h1>
+    <p><strong>Titulaire du compte:</strong> Alain Rodriguez</p>
+    <p><strong>Solde du compte:</strong> 500,000€</p>
+</div>
+
+</body>
+</html>
